b56ce17ab2c97ad1fec11e1e599d57b15bd7dc89
Orange-Murker <23561952+Orange-Murker@users.noreply.github.com>
2022-08-14 17:01:41 +0200

    Added the code and updated README

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Restroom Privacy Mode
 Mute your bodily sounds by playing random music.
 
-A little fun project that I made in under a day.
+<img src="images/privacy-no-box.jpg" width=40% height=40% alt="An image of the project without a box">
+
+A fun little project that I made in under a day.
 
 ---
 
@@ -18,13 +20,66 @@ A little fun project that I made in under a day.
 
 ### Setup Process and Assembly Instructions
 
-Coming Soon
+#### Software Setup
+
+1. Flash the Raspberry Pi Pico with `CircuitPython` using the `.uf2` file from
+[here](https://circuitpython.org/board/raspberry_pi_pico/)
+
+2. Simply drag the `.uf2` file onto the mass storage device while your Pico is in
+the bootloader. You can enter the bootloader by holding the `BOOTSEL` button.
+
+3. A `CIRCUITPY` storage device should appear on your computer. Drag the `code.py` file from this repository
+onto it, and you should be good to go!
+
+#### Hardware setup
+
+##### DAC
+
+`RPi Pico` -- `MAX98357`
+
+`VBUS` -- `Vin`
+
+`GND` -- `GND`
+
+`GP0` -- `BCLK`
+
+`GP1` -- `LRC`
+
+`GP2` -- `DIN`
+
+##### SD Card
+
+`RPi Pico` -- `SD Card`
+
+`3.3V` -- `3.3V`
+
+`GP10` -- `CLK`
+
+`GP11` -- `MOSI`
+
+`GP12` -- `MISO`
+
+`GP13` -- `CS`
+
+`GND` -- `GND`
+
+##### BIG RED BUTTON
+
+`RPi Pico` -- `BIG RED BUTTON`
+
+`GND` -- `COM`
+
+`GP6` -- `NO`
+
+`VBUS` -- `LED`
 
 ---
 
 ### Usage instructions
 Press the button to play a randomly selected track from the SD card.
+
 Hold the button to stop playback.
+
 It is that simple!
 
 ---
---
+++ code.py
@@ -0,0 +1,70 @@
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import busio
+import sdcardio
+import audiobusio
+import storage
+import audiocore
+import os
+import random
+import time
+
+# Opens and returns a random file from a list of files
+def get_random_file(files):
+    random_name = "/sd/" + files[random.randrange(len(files))]
+    return open(random_name, "rb")
+
+# SPI for the SD card
+spi = busio.SPI(board.GP10, board.GP11, board.GP12)
+cs = board.GP13
+
+# Button
+button = DigitalInOut(board.GP6)
+button.direction = Direction.INPUT
+button.pull = Pull.UP
+
+sdcard = sdcardio.SDCard(spi, cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
+# All the .wav files in the root directory
+files = list(filter(lambda f: f.endswith(".wav"), os.listdir("/sd")))
+file = get_random_file(files)
+
+# Open the I2S device
+speaker = audiobusio.I2SOut(board.GP0, board.GP1, board.GP2)
+
+# Time in seconds
+DEBOUNCE_DELAY = 0.02
+HOLD_DELAY = 1.0
+last_time = time.monotonic()
+previous_button_value = True
+hold_triggered = False
+while True:
+    current_time = time.monotonic()
+    current_button_value = button.value
+
+    if current_button_value != previous_button_value:
+        # Check for button release
+        if current_time - last_time > DEBOUNCE_DELAY and current_button_value:
+            if current_time - last_time < HOLD_DELAY:
+                # Close the file that is currently open to avoid IOError
+                file.close()
+
+                file = get_random_file(files)
+
+                # Process the wav file
+                file_playback = audiocore.WaveFile(file)
+
+                speaker.play(file_playback)
+
+            # Button now is up. We can reset the hold trigger state
+            hold_triggered = False
+
+        # Update previous values on every state change
+        previous_button_value = current_button_value
+        last_time = current_time
+
+    # Check whether the hold was triggered before to avoid triggering it on the next loop
+    if not hold_triggered and not current_button_value and current_time - last_time > HOLD_DELAY:
+        hold_triggered = True
+        speaker.stop()
